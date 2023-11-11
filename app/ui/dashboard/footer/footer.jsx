bd59ef884f4f4228d58507e73f28ad4946bd472f
Christine Wong <jumiryoo@gmail.com>
2023-11-11 14:15:33 -0800

    updates

---
+++ app/ui/dashboard/footer/footer.jsx
@@ -0,0 +1,12 @@
+import styles from './footer.module.css'
+
+const Footer = () => {
+    return(
+        <div className={styles.container}>
+            <div className={styles.logo}>Christine Wong</div>
+
+        </div>
+    )
+}
+
+export default Footer
