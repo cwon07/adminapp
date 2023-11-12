8e624f293026b7901cb7030c3a2ca82fc212cdac
Christine Wong <jumiryoo@gmail.com>
2023-11-12 13:35:21 -0800

    connecting to database

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -3,8 +3,13 @@ import Search from '@/app/ui/dashboard/search/search'
 import Link from 'next/link'
 import Image from 'next/image'
 import Pagination from '@/app/ui/dashboard/pagination/pagination'
+import { fetchUsers } from '@/app/lib/data'
+import { deleteUser } from '@/app/lib/actions'
 
-const UsersPage = () => {
+const UsersPage = async () => {
+   const users = await fetchUsers();
+
+    console.log(users)
     return(
         <div className={styles.container}>
             <div className={styles.top}>
@@ -25,29 +30,36 @@ const UsersPage = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>
-                            <div className={styles.user}>
-                                <Image src="/blank_avatar.png" alt="" width={40} height={40} className={styles.userImage} />
-                            John Doe
-                            </div>
-                        </td>
-                        <td>john@gmail.com</td>
-                        <td>13.01.2022</td>
-                        <td>Admin</td>
-                        <td>active</td>
-                        <td>
-                            <div className={styles.buttons}>
-                                <Link href="/dashboard/users/test">
-                                    <button className={`${styles.button} ${styles.view}`}>View</button>
-                                </Link>
-                                    <button className={`${styles.button} ${styles.delete}`}>Delete</button>
-                            </div>
-                        </td>
-                    </tr>
+                    {users.map((user) => (
+                        <tr key={user.id}>
+                            <td>
+                                <div className={styles.user}>
+                                    <Image src={user.img || "/blank_avatar.png"} alt="" width={40} height={40} className={styles.userImage} />
+                                {user.username}
+                                </div>
+                            </td>
+                            <td>{user.email}</td>
+                            <td>{user.createdAt?.toString().slice(4, 16)}</td>
+                            <td>{user.isAdmin ? "Admin" : "Client"}</td>
+                            <td>{user.isActive ? "active" : "inactive"}</td>
+                            <td>
+                                <div className={styles.buttons}>
+                                    <Link href={`/dashboard/users/${user.id}`}>
+                                        <button className={`${styles.button} ${styles.view}`}>View</button>
+                                    </Link>
+                                        <form action={deleteUser}>
+                                            <input type="hidden" name="id" value={(user.id)} />
+                                            <button className={`${styles.button} ${styles.delete}`}>
+                                                Delete
+                                            </button>
+                                        </form>
+                                </div>
+                            </td>
+                        </tr>
+                    ))}                                         
                 </tbody>
             </table>
-            <Pagination/>
+            <Pagination />
         </div>
     )
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol:"http",
+                hostname:"www.quailbellmagazine.com"
+            }
+        ]
+    }
+}
 
 module.exports = nextConfig
