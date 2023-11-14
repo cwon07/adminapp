c19424c5fba365cef52caae028b33eef3b85effc
Christine Wong <jumiryoo@gmail.com>
2023-11-14 00:26:38 -0800

    updated the login page

--- app/lib/actions.js
+++ app/lib/actions.js
@@ -153,3 +153,14 @@ export const deleteProduct = async (formData) => {
   revalidatePath("/dashboard/products")
   redirect("/dashboard/products")
 }
+
+export const authenticate = async (formData) => {
+  const {username, password} = Object.fromEntries(formData)
+
+  try {
+    await signIn("credentials", {username, password});
+  }catch(err){
+    console.log(err);
+    throw err
+  }
+}
